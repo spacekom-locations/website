1fd1b34469497a943220ca8450054bbb99e24635
Ayman Nageeb <aimangta123121@gmail.com>
2022-05-22 10:17:04 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <main-app-bar />
+
+    <v-main>
+      
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import MainAppBar from './components/appbar/MainAppBar.vue';
+
+
+export default {
+  name: 'App',
+
+  components: {
+    MainAppBar
+    
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/appbar/MainAppBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar app color="white" dark>
+    <div class="d-flex align-center">
+      <v-img
+        alt="Wayn Logo"
+        class="shrink mr-2"
+        contain
+        :src="require('@/assets/wayn-mini-logo.png')"
+        transition="scale-transition"
+        width="40"
+        route
+        :to="{ name: 'Home' }"
+      />
+      <span class="blue--text display-1 font-weight-bold"> Wayn </span>
+    </div>
+
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
