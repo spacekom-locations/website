18085754ef54e22a46c1b70c9c4cebcc263be8f6
Ayman Nageeb <aimangta123121@gmail.com>
2022-05-26 01:19:09 +0200

    test: github action autodeployment 2

--- server_deploy.sh
+++ server_deploy.sh
@@ -2,46 +2,19 @@
 
 set -e
 
-echo "Deploying SpaceKom API ..."
+echo "Deploying SpaceKom Website ..."
 
 # Enter maintenance mode
-
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
 
     # Update codebase
 
     git fetch origin deploy
 
     git reset --hard origin/deploy
-
-
-    # Install dependencies based on lock file
-
-    composer install --no-interaction --prefer-dist --optimize-autoloader
-
-
-    # Migrate database
-
-    php artisan migrate --force
     
     chmod -R 777 .
 
 
-    # Note: If you're using queue workers, this is the place to restart them.
+# Exit maintenance 
 
-    # ...
-
-    # Clear cache
-
-    php artisan optimize
-
- 
-    # Reload PHP to update opcache
-
-    echo "" | sudo -S service php8.1-fpm reload
-
-# Exit maintenance mode
-
-php artisan up
-
-echo "SpaceKom API deployed!"
+echo "SpaceKom Website deployed!"
